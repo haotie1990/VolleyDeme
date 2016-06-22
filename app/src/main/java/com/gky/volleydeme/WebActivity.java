c7bafcfeb9aac98b845e0e06d232e97ec89e6e8e
haotie1990 <haotie1990@foxmail.com>
2016-06-22 18:04:03 +0800

    添加网页浏览activity的toolbar返回功能

--- app/src/main/java/com/gky/volleydeme/WebActivity.java
+++ app/src/main/java/com/gky/volleydeme/WebActivity.java
@@ -6,8 +6,6 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
@@ -35,6 +33,14 @@ public class WebActivity extends AppCompatActivity{
         setContentView(R.layout.activity_web);
         ButterKnife.bind(this);
         setSupportActionBar(mToolbar);
+
+        mToolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
 
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
@@ -64,28 +70,6 @@ public class WebActivity extends AppCompatActivity{
             mWebView.loadUrl(url);
             getSupportActionBar().setTitle(desc);
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
